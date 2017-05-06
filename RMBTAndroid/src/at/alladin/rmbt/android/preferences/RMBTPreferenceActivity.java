b12607f549bcf98a60aedfa7a8fd1bf9d3f1e5c1
Dietmar Zlabinger (mc) <dietmar.zlabinger@rtr.at>
2017-05-06 19:13:01 +0200

    RMBTAndroid: Add Speed-Info to startpage (#859)

--- RMBTAndroid/src/at/alladin/rmbt/android/preferences/RMBTPreferenceActivity.java
+++ RMBTAndroid/src/at/alladin/rmbt/android/preferences/RMBTPreferenceActivity.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright 2013-2016 alladin-IT GmbH
  * Copyright 2016-2017 Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
