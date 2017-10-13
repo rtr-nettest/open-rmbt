d360438040ae6553b9150d605446c30e4d1eec0e
Thomas Schreiber <thomas.schreiber@rtr.at>
2017-10-13 15:19:09 +0200

    #826 update copyright notice

--- RMBTStatisticServer/src/at/rtr/rmbt/statisticServer/opendata/HistogramResource.java
+++ RMBTStatisticServer/src/at/rtr/rmbt/statisticServer/opendata/HistogramResource.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright 2015, 2016 Thomas Schreiber
- * Copyright 2015, 2016 Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)
+ * Copyright 2015 Rundfunk und Telekom Regulierungs-GmbH (RTR-GmbH)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
